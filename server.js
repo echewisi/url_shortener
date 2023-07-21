9f93ba1b84a7f2c44bdb4b33e255e110d4793572
echewisi <josephechewisi@gmail.com>
2023-07-22 00:33:04 +0100

    fin

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const mongoose= require("mongoose");
-const ShortUrl= require("./models/shorturls")
+const ShortUrl= require("./models/shorturls");
+const shorturls = require("./models/shorturls");
 const app= express();
 
 mongoose.connect('mongodb://localhost/url3030',{
@@ -17,7 +18,23 @@ app.get("/", async (req, res)=>{
     res.render("index", {shorturls: shorturls})
 })
 
+
+
 app.post("/shorturls", async (req, res)=>{
     await ShortUrl.create({full:req.body.fullUrl})
     res.redirect('/')
+})
+
+app.get("/:shortUrl", async (req,res)=>{
+    const shortUrl= await ShortUrl.findOne(req.params.shortUrl)
+    if(shortUrl== null){
+        return res.sendStatus(404)
+    }
+    else{
+        shortUrl.clicks++
+        shortUrl.save()
+
+        return res.redirect(shortUrl.full)
+    }
+
 })
